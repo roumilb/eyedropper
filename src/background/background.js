0e9256241689f731b580ad5b9ffd110a09f8a656
Rémi Leclercq <leclercq.remi@gmail.com>
2020-11-23 15:45:27 +0100

    prevent from core pages

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,23 @@
 chrome.runtime.onConnect.addListener(function (port) {
-    if (port.name === 'eyedropp_color_picker') takePicture(port);
+    if (port.name === 'eyedropp_color_picker') {
+        takePicture(port);
+    } else if (port.name === 'eyedropp_color_picker_setup') {
+        checkUrlCurrentTab(port);
+    }
 });
+
+function checkUrlCurrentTab(port) {
+    chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    }, function (tabs) {
+        // and use that tab to fill in out title and url
+        let tab = tabs[0];
+        const regex = /https|http/;
+        const validUrl = regex.exec(tab.url);
+        port.postMessage({data: validUrl !== null});
+    });
+}
 
 function takePicture(port) {
     chrome.tabs.query({
--- src/popup/index.js
+++ src/popup/index.js
@@ -13,5 +13,8 @@ document.getElementById('button_color_picker').addEventListener('click', () => {
 let port = chrome.runtime.connect({name: 'eyedropp_color_picker_setup'});
 port.postMessage({data: 'can_pick'});
 port.onMessage.addListener(function (res) {
-    console.log(res.data);
+    if (!res.data) {
+        document.querySelector('#color_picker_alert').style.display = 'block';
+        document.querySelector('#button_color_picker').setAttribute('disabled', 'true');
+    }
 });
